94378518289c14c2ed9a56c9a13abcb57f0963fa
Amadeo Bon <amadeosph@outlook.com>
2025-09-15 09:35:49 -0300

    fix import path for TradingViewCard component

--- components/page_addition.tsx
+++ components/page_addition.tsx
@@ -1,6 +1,6 @@
 // No topo do arquivo
 import dynamic from 'next/dynamic';
-const TradingViewCard = dynamic(() => import('./components/TradingViewCard'), { ssr: false });
+const TradingViewCard = dynamic(() => import('../components/ui/TradingViewCard'), { ssr: false });
 
 // ... dentro do componente da página, logo abaixo dos cards existentes:
 <TradingViewCard />
